3d88f8484f56c728cc038c624e449992dafb92b3
Anderson Reyes <reyesanderson428@gmail.com>
2017-09-01 15:45:14 -0400

    Added extra layer

--- src/convnet.py
+++ src/convnet.py
@@ -68,7 +68,7 @@ def conv2d(x, filter_shape):
     """
     assert len(filter_shape) == 4
     W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.03))
-    B = tf.Variable(tf.constant(0.1), [filter_shape[-1]])
+    B = tf.Variable(tf.constant(0.1, shape=[filter_shape[-1]]))
     layer = tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME') + B
     return layer
 
@@ -80,7 +80,7 @@ def relu(x):
 def fully_connected(x, shape):
     assert len(shape) == 2
     W = tf.Variable(tf.truncated_normal(shape, stddev=0.03))
-    B = tf.Variable(tf.constant(0.1), [shape[-1]])
+    B = tf.Variable(tf.constant(0.1, shape=[shape[-1]]))
     X_flat = tf.reshape(x, [-1, shape[0]])
     layer = relu(tf.matmul(X_flat, W) + B)
     return layer
@@ -89,7 +89,7 @@ def fully_connected(x, shape):
 def output_layer(x, shape):
     assert len(shape) == 2
     W = tf.Variable(tf.truncated_normal(shape, stddev=0.03))
-    B = tf.Variable(tf.constant(0.1), [shape[-1]])
+    B = tf.Variable(tf.constant(0.1, shape=[shape[-1]]))
     layer = tf.matmul(x, W) + B
     return layer
 
@@ -107,29 +107,29 @@ assert len(X_all) == len(Y_all)
 
 # save 20% for testing
 test_start = int(.80 * len(X_all))
-X_train, Y_train = X_all[:test_start], Y_all[:test_start]
-X_test, Y_test = X_all[test_start:], Y_all[test_start:]
+X_train, Y_train = X_all[:test_start, :], Y_all[:test_start, :]
+X_test, Y_test = X_all[test_start:, :], Y_all[test_start:, :]
 
 alpha = 0.0001
 epochs = 10
 batch_size = 50
 
-X = tf.placeholder(dtype=tf.float32, shape=[None, 2304], name='Input')
-X_shaped = tf.reshape(X, [-1, 48, 48, 1])
-Y = tf.placeholder(dtype=tf.float32, shape=[None, 7], name='Output')
+x = tf.placeholder(dtype=tf.float32, shape=[None, 2304], name='Input')
+x_shaped = tf.reshape(x, [-1, 48, 48, 1])
+y = tf.placeholder(dtype=tf.float32, shape=[None, 7], name='Output')
 
-conv_layer1 = relu(conv2d(X_shaped, [5, 5, 1, 32]))
+conv_layer1 = relu(conv2d(x_shaped, [5, 5, 1, 32]))
 max_pool_layer1 = max_pool(conv_layer1, [2, 2])
-conv_layer2 = relu(conv2d(conv_layer1, [5, 5, 32, 64]))
+conv_layer2 = relu(conv2d(max_pool_layer1, [5, 5, 32, 64]))
 max_pool_layer2 = max_pool(conv_layer2, [2, 2])
 conv_layer3 = relu(conv2d(max_pool_layer2, [5, 5, 64, 128]))
 max_pool_layer3 = max_pool(conv_layer3, [2, 2])
-fc_l3 = fully_connected(max_pool_layer2, [12 * 12 * 128, 1000])
-outlayer = output_layer(fc_l3, [1000, 7])
+fc_layer4 = fully_connected(max_pool_layer3, [6 * 6 * 128, 1000])
+y_predict = output_layer(fc_layer4, [1000, 7])
 
-entropy_cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=outlayer, labels=Y))
+entropy_cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_predict, labels=y))
 optimiser = tf.train.AdamOptimizer(learning_rate=alpha).minimize(entropy_cost)
-correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(outlayer, 1))
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_predict, 1))
 accurary = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 init_op = tf.global_variables_initializer()
@@ -144,17 +144,17 @@ with tf.Session() as sess:
         avg_cost = 0
 
         for i in range(n_batches):
-            idx = i*n_batches
-            batch_x = X_train[idx: idx + n_batches]
-            batch_y = Y_train[idx: idx + n_batches]
-            _, result = sess.run([optimiser, entropy_cost], feed_dict={X: batch_x, Y: batch_y})
+            idx = i*batch_size
+            batch_x = X_train[idx: idx + batch_size]
+            batch_y = Y_train[idx: idx + batch_size]
+            _, result = sess.run([optimiser, entropy_cost], feed_dict={x: batch_x, y: batch_y})
             avg_cost += result / n_batches
         
-        test_accuracy = sess.run(accurary, feed_dict={X: X_test, Y: Y_test})
+        test_accuracy = sess.run(accurary, feed_dict={x: X_test, y: Y_test})
         print('Epoch:', (epoch + 1), 'cost = ', '{:.3f}'.format(avg_cost),
         ' test accuracy: {:.3f}'.format(test_accuracy))
 
     print('\nTraining Complete')
-    print('accurary:', sess.run(accurary, feed_dict={X: X_test, Y: Y_test}))
+    print('accurary:', sess.run(accurary, feed_dict={x: X_test, y: Y_test}))
 
 writer.close()
